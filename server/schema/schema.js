4cbc816c11bc177962c6a9a1bd6d905cb0374547
Nikhil Shende <nkhlshnd99@gmail.com>
2019-12-07 18:04:05 -0500

    Nested Queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,21 +1,49 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList} = graphql;
 
-var testData = [
-    {id: '1', fName: 'James', lName: 'Harden'},
-    {id: '2', fName: 'Kawhi', lName: 'Leonard'},
-    {id: '3', fName: 'Jimmy', lName: 'Butler'}
+var playersData = [
+    {id: '1', fName: 'James', lName: 'Harden', teamId: '1'},
+    {id: '2', fName: 'Kawhi', lName: 'Leonard', teamId: '2'},
+    {id: '3', fName: 'Jimmy', lName: 'Butler', teamId: '3'},
+    {id: '4', fName: 'Russell', lName: 'Westbrook', teamId:'1'}
 ];
 
+var teamsData = [
+    {id:'1', city:'Houston', name: 'Rockets'},
+    {id:'2', city:'Los Angeles', name: 'Clippers'},
+    {id:'3  ', city:'Miami', name: 'Heat'}
+];
 
 const PlayerType = new GraphQLObjectType({
     name:'Player',
     fields:() => ({
-        id: {type:GraphQLString},
+        id: {type:GraphQLID},
         fName: {type: GraphQLString},
-        lName: {type: GraphQLString}
+        lName: {type: GraphQLString},
+        teamId: {type: GraphQLID},
+        team: { 
+            type: TeamType,
+            resolve(parent, args){
+                return _.find(teamsData,{id: parent.teamId})
+            }
+        }
+    })
+});
+
+const TeamType = new GraphQLObjectType({
+    name:'Team',
+    fields:() => ({
+        id: {type:GraphQLID},
+        city: {type: GraphQLString},
+        name: {type: GraphQLString},
+        players: {
+            type: new GraphQLList(PlayerType),
+            resolve(parent, args){
+                return _.filter(playersData, {teamId: parent.id})
+            }
+        }
     })
 });
 
@@ -24,10 +52,18 @@ const RootQuery = new GraphQLObjectType({
     fields:{
         player: {
             type: PlayerType,
-            args: {id: {type:GraphQLString}},
+            args: {id: {type:GraphQLID}},
             resolve(parent, args){
                 //code to get data from db
-                return _.find(testData,{id:args.id});
+                return _.find(playersData,{id:args.id});
+            }
+        },
+        team: {
+            type: TeamType,
+            args: {id: {type:GraphQLID}},
+            resolve(parent, args){
+                //code to get data from db
+                return _.find(teamsData,{id:args.id});
             }
         }
     }
